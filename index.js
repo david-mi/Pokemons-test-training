14b06a5f71d561f124cc328eb9a9ee0bfafcacb1
David Michel <david.michel2809@gmail.com>
2022-11-26 09:25:58 +0100

    feat: adding intersectionObserver to show more pokemon cards when scrolling

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 import { createAndDisplayPokemonCards } from "./view.js";
+
+const loaderElement = document.querySelector(".loader");
+let isFetchingData = false;
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(async ({ isIntersecting }) => {
+    if (isIntersecting && !isFetchingData) {
+      isFetchingData = true;
+      createAndDisplayPokemonCards();
+      isFetchingData = false;
+    }
+  });
+}, { rootMargin: "-50px" });
+
+
 (async () => {
   createAndDisplayPokemonCards();
+  observer.observe(loaderElement);
 })();
