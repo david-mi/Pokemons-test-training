a78c5e709ecf40ad2a17066e85e77c0b08227501
David Michel <david.michel2809@gmail.com>
2022-11-26 14:56:57 +0100

    feat: hiding previous clicked card if different

--- handlers.js
+++ handlers.js
@@ -1,7 +1,14 @@
+let previousCard = null;
+
 export const handleCardClick = ({ currentTarget }) => {
+  if (previousCard?.classList.contains("turn") && previousCard !== currentTarget) {
+    handleCardClick({ currentTarget: previousCard });
+  }
+
   const faceCard = currentTarget.querySelector(".face-card");
   const statsCard = currentTarget.querySelector(".stats-card");
   currentTarget.classList.toggle("turn");
   faceCard.classList.toggle("hide");
   statsCard.classList.toggle("show");
+  previousCard = currentTarget;
 };
