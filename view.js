a4b08ecdb5c18e201555a7a3b028f83a593a957f
David Michel <david.michel2809@gmail.com>
2022-11-26 10:18:52 +0100

    feat: rework card creation

--- view.js
+++ view.js
@@ -20,38 +20,37 @@ const createPokemonStatsCard = (stats) => {
       `;
   }, "");
 
-  return `
-  <div class="stats-card">
-    ${statsHtml}
-  </div>
-  `;
+  const pokemonStatsCard = document.createElement("div");
+  pokemonStatsCard.classList.add("stats-card");
+  pokemonStatsCard.innerHTML = statsHtml;
+
+  return pokemonStatsCard;
 };
 
 const createPokemonFaceCard = ({ id, image, name }) => {
-  return `
-  <div class="face-card">
+  const pokemonFaceCard = document.createElement("div");
+  pokemonFaceCard.classList.add("face-card");
+  pokemonFaceCard.innerHTML = `
     <span>${id}.${name}</span>
     <img src=${image}>
-  </div>
-`;
+  `;
+
+  return pokemonFaceCard;
 };
 
-export const createPokemonCardHtml = (pokemon) => {
+const createPokemonCard = (pokemon) => {
   const cardElement = document.createElement("div");
   cardElement.classList.add("card");
-  cardElement.innerHTML = `
-    ${createPokemonFaceCard(pokemon)}
-    ${createPokemonStatsCard(pokemon.stats)}
-  `;
+
+  cardElement.append(createPokemonFaceCard(pokemon), createPokemonStatsCard(pokemon.stats));
   return cardElement;
 };
-
 
 export const createAndDisplayPokemonCards = async () => {
   const pokemonsData = await getPokemons();
 
   pokemonsData.forEach(pokemonData => {
-    const pokemonCard = createPokemonCardHtml(pokemonData);
+    const pokemonCard = createPokemonCard(pokemonData);
     cardsElement.insertAdjacentElement("beforeend", pokemonCard);
   });
 };
